2f0a04ed4f383f34a0d648d699d977def62a182e
Ale <52315680+Aalleexx-87@users.noreply.github.com>
2026-05-31 19:06:06 +0200

    Update solax_inverter.py

--- solax_inverter.py
+++ solax_inverter.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 import paho.mqtt.client as mqtt
 from solax.inverters import X1HybridGen4
+from solax import discover
 
 with open("/data/options.json") as f:
     config = json.load(f)
@@ -28,7 +29,7 @@ def send_mqtt(client, data):
 
 async def main():
     print(f"🔧 Connecting to inverter {ip_inverter}:{port_inverter}")
-    inverter = X1HybridGen4._build(ip_inverter, port_inverter, password_inverter, request_timeout=30)
+    inverter = X1HybridGen4._build(ip_inverter, port_inverter, password_inverter)
     print(f"✅ Inverter: {inverter.__class__.__name__}")
 
     client = mqtt.Client()
@@ -40,7 +41,8 @@ async def main():
 
     while True:
         try:
-            data = await inverter.get_data()
+            async with asyncio.timeout(30):
+                data = await inverter.get_data()
             if isinstance(data, list):
                 data = data[0]
             print("📡 RAW DATA:")
