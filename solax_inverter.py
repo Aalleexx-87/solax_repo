8d8b05d075719bb426aa5c62fc2e2a5f8a7318d2
Ale <52315680+Aalleexx-87@users.noreply.github.com>
2026-05-31 19:37:45 +0200

    Update solax_inverter.py

--- solax_inverter.py
+++ solax_inverter.py
@@ -2,8 +2,8 @@ import asyncio
 import json
 import paho.mqtt.client as mqtt
 # from solax import RealTimeAPI, X3Hybrid
-import solax
-print(f"solax version: {solax.__version__}")
+import importlib.metadata
+print(f"solax version: {importlib.metadata.version('solax')}")
 
 with open("/data/options.json") as f:
     config = json.load(f)
